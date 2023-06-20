383f9038a22c06c3d61c089c93fb01421b3f1b69
Romario Arnat <Romario.Arnat@green.com.na>
2023-06-20 11:30:35 +0200

    updated the code

--- Send.py
+++ Send.py
@@ -6,24 +6,16 @@ import random
 
 # Set the messages
 messages = [  
-    "Good morning! Wishing you a day full of sunshine and happiness.",
-    "Rise and shine! It's a new day full of opportunities.",
-    "Good morning! May your day be filled with love and laughter.",
-    "A beautiful day starts with a beautiful mindset. Good morning!",
-    "Good morning! Let's make today amazing.",
-    "Good morning! Today is a new beginning.",
-    "Good morning! May your day be as wonderful as you are.",
-    "Good morning! Let's make the most of this beautiful day.",
-    "Good morning! May your day be filled with joy and blessings.",
-    "Good morning! Here's to a great day ahead.",
-    'Good morning, angel. I hope you dreamed the sweetest dreams.',
-'Hope your morning is as bright and beautiful as you are.',
-'You were the first person I thought of when I woke up, and it made me smile.',
-'Dang, girl—they don’t call it “beauty sleep” for nothing! You just wake up this gorgeous?',
-'Everyone should get to experience a sunrise with you.',
-'Ah, you’re finally awake, my sleeping beauty!',
-'For me, the sun only rises when you smile in the morning and sets when you close your eyes at night.'
+    "Good morning to everyone except the person who stole my coffee mug.",
+    "Good morning sunshine! Just kidding, it's raining cats and dogs.",
+    "Good morning world! I'm ready to face the day with a smile and a positive attitude. And a lot of caffeine.",
+    "Good morning! I hope you slept well. Because I didn't. Thanks for snoring all night.",
+
+    "Good morning! Today is a great day to be alive. Unless you have a hangover. Then it's a terrible day.",
     ]
+
+#enter your numner here
+number = ("+264811234567")
 
 # Send the message
 for i in range(24):
@@ -32,19 +24,12 @@ for i in range(24):
 
 
 
-    kit.sendwhatmsg("+264817500205", message, 10, 59,  tab_close=True)
+    kit.sendwhatmsg(number, message, 11, 25,  tab_close=True)
 
-    # kit.sendwhatmsg("+264812413654", message, 7, 31,  tab_close=True)
-    # time.sleep(10)
-
-    
-    # kit.sendwhatmsg("+264816326442", message, 7, 32,  tab_close=True)
-    # kit.sendwhatmsg("+264817700626", message, 7, 33,  tab_close=True)
+ 
     # Close the tab +
     pyautogui.hotkey('ctrl', 'w')
+    # wait 24 hours before running the script again 
     time.sleep(3600)
-
-# Wait for 10 seconds after sending the message
-# time.sleep(10)
 
 
