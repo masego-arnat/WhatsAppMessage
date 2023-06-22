4265d367a7ed1d25b2b0f5172e23db71541ff2b1
Romario Arnat <Romario.Arnat@green.com.na>
2023-06-22 09:27:09 +0200

    dfrgdfgdfg

--- Send.py
+++ Send.py
@@ -14,22 +14,20 @@ messages = [
     "Good morning! Today is a great day to be alive. Unless you have a hangover. Then it's a terrible day.",
     ]
 
-#enter your numner here
-number = ("+264811234567")
+
+number = "+264811234567"  # Phone number to send the message to
 
 # Send the message
 for i in range(24):
- 
-    message = random.choice(messages)
+    message = random.choice(messages)  # Select a random message from the list of messages
 
+    # Send the message using the 'sendwhatmsg' function from the 'pywhatkit' library
+    # Parameters: number (phone number), message (text message), hour, minute, tab_close (optional, closes the browser tab)
+    kit.sendwhatmsg(number, message, 11, 25, tab_close=True)
 
-
-    kit.sendwhatmsg(number, message, 11, 25,  tab_close=True)
-
- 
-    # Close the tab +
+    # Close the tab using the 'hotkey' function from the 'pyautogui' library
     pyautogui.hotkey('ctrl', 'w')
-    # wait 24 hours before running the script again 
-    time.sleep(3600)
 
+    # Wait for 24 hours (3600 seconds per hour) before running the script again
+    time.sleep(3600)
 
